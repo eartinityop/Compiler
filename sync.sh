9ab11df7dccf1d26032c8aa0eb5450b1c597fecb
Pranav Jha <77088825+pranavjha21@users.noreply.github.com>
2023-09-28 20:10:40 +0530

    Compiler: Added manifest and local_manifest flags

--- sync.sh
+++ sync.sh
@@ -8,9 +8,9 @@ echo "Checking System Configuration"
 chk
 
 sync() {
-repo init --depth=1 --no-repo-verify -u $ROMREPO
-git clone $LOCALMANIFEST .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u $MANIFEST
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle -j$(nproc --all)
+git clone $LOCAL_MANIFEST .repo/local_manifests
 }
 echo "Syncing Rom & Device Sources"
 sync
