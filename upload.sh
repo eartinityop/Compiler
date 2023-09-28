354d3095e424fcfa835ba1a4865282ab32e00e84
Pranav Jha <77088825+pranavjha21@users.noreply.github.com>
2023-09-28 21:31:22 +0530

    Compiler: Trigger build

--- upload.sh
+++ upload.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-rccache() {
-cd /tmp
-rm ccache.tar.gz
-}
-echo "Removing Old CCACHE"
-rccache
-
 compress() {
 time tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1 ccache 1
 }
@@ -19,11 +12,3 @@ curl -sL https://git.io/file-transfer | sh
 }
 echo "Uploading CCACHE"
 uccache
-
-rom_upload() {
-cd ~/
-curl --upload-file .$OUTPUT https://free.keep.sh > link0.txt && cat link0.txt
-curl --upload-file .$OUTPUT https://free.keep.sh > link1.txt && cat link1.txt
-}
-echo "Uploading Rom & Boot From /Out"
-rom_upload
